314fe0860c83e6de5ef633c8699b892a6c273b4b
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-11-27 18:23:07 +0100

    Load strategy default config from application.

--- lib/ravenx.ex
+++ lib/ravenx.ex
@@ -1,23 +1,29 @@
 defmodule Ravenx do
 
-  def dispatch(strategy, [title: _t, short_body: _sb, body: _b] = payload, options \\ []) do
+  def dispatch(strategy, [title: _t, body: _b] = payload, options \\ []) do
     handler = available_strategies
     |> Keyword.get(strategy)
 
+    opts = options
+    |> merge_options(strategy)
+
     unless (is_nil(handler)) do
-      Task.async(fn -> handler.call(payload, options) end)
+      Task.async(fn -> handler.call(payload, opts) end)
       |> Task.await()
     else
       {:error, "#{strategy} strategy not defined"}
     end
   end
 
-  def dispatch_async(strategy, [title: _t, short_body: _sb, body: _b] = payload, options \\ []) do
+  def dispatch_async(strategy, [title: _t, body: _b] = payload, options \\ []) do
     handler = available_strategies
     |> Keyword.get(strategy)
 
+    opts = options
+    |> merge_options(strategy)
+
     unless (is_nil(handler)) do
-      pid = Task.async(fn -> handler.call(payload, options) end)
+      pid = Task.async(fn -> handler.call(payload, opts) end)
       {:ok, pid}
     else
       {:error, "#{strategy} specified not defined"}
@@ -28,5 +34,10 @@ defmodule Ravenx do
     [
       slack: Ravenx.Strategy.Slack
     ]
+  end
+
+  def merge_options(opts, strategy) do
+    Application.get_env(:ravenx, strategy, [])
+    |> Keyword.merge(opts)
   end
 end
