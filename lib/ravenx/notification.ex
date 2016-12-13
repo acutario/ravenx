ce4b6938388fb79d52b4ecbeb03321c40e0559b7
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-12-13 22:27:25 +0100

    Add async methods.

--- lib/ravenx/notification.ex
+++ lib/ravenx/notification.ex
@@ -12,6 +12,12 @@ defmodule Ravenx.Notification do
         |> get_notifications_list
         |> Enum.map(&Ravenx.Notification.dispatch_notification/1)
       end
+
+      def dispatch_async(opts) do
+        opts
+        |> get_notifications_list
+        |> Enum.map(&Ravenx.Notification.dispatch_async_notification/1)
+      end
     end
   end
 
@@ -21,6 +27,19 @@ defmodule Ravenx.Notification do
         Ravenx.dispatch(strategy, payload, options)
       [strategy, payload] when is_atom(strategy) and is_map(payload) ->
         Ravenx.dispatch(strategy, payload)
+      [_] ->
+        {:error, "Notification config must include, at least, an strategy atom and a payload map."}
+      _ ->
+        {:error, "Notification config not valid"}
+    end
+  end
+
+  def dispatch_async_notification(notification) when is_list(notification) and length(notification) >=2 do
+    case notification do
+      [strategy, payload, options] when is_atom(strategy) and is_map(payload) and is_map(options) ->
+        Ravenx.dispatch_async(strategy, payload, options)
+      [strategy, payload] when is_atom(strategy) and is_map(payload) ->
+        Ravenx.dispatch_async(strategy, payload)
       [_] ->
         {:error, "Notification config must include, at least, an strategy atom and a payload map."}
       _ ->
