5f25fa6492bb61fd56e28e83946208c363c119fa
belaustegui <cristian.alvb@gmail.com>
2017-03-01 17:43:43 +0100

    Remove duplicated code
    Instead of duplicating `dispatch` and `dispatch_async` code, we use
    `dispatch_async` as the canonical implementation. `dispatch` calls
    `dispatch_async` and waits synchronously for the task to finish.

--- lib/ravenx.ex
+++ lib/ravenx.ex
@@ -31,14 +31,7 @@ defmodule Ravenx do
   """
   @spec dispatch(notif_strategy, notif_payload, notif_options) :: notif_result
   def dispatch(strategy, payload, options \\ %{}) do
-    handler = Keyword.get(available_strategies(), strategy)
-
-    opts = get_options(strategy, payload, options)
-
-    if is_nil(handler) do
-      {:error, {:unknown_strategy, strategy}}
-    else
-      task = Task.async(fn -> handler.call(payload, opts) end)
+    with {:ok, task} <- dispatch_async(strategy, payload, options) do
       Task.await(task)
     end
   end
