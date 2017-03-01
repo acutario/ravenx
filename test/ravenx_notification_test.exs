a518fab33a29d88725d3502624fc7f28eea6d434
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2017-03-01 09:27:15 +0100

    Add async notification dispatching tests.

--- test/ravenx_notification_test.exs
+++ test/ravenx_notification_test.exs
@@ -19,4 +19,31 @@ defmodule RavenxNotificationTest do
     dummy_not_result = Keyword.get(result, :dummy_not)
     assert dummy_not_result == DummyStrategy.get_error_result
   end
+
+  test "dispatch multiple notifications asynchronously returns expected keys" do
+    result = Ravenx.Test.TestNotification.dispatch_async(true)
+
+    assert Keyword.has_key?(result, :dummy)
+    assert Keyword.has_key?(result, :dummy_not)
+  end
+
+  test "dispatch multiple notifications asynchronously returns expected tasks" do
+    result = Ravenx.Test.TestNotification.dispatch_async(true)
+
+    dummy_result = Keyword.get(result, :dummy)
+    assert {:ok, %Task{}} = dummy_result
+
+    dummy_not_result = Keyword.get(result, :dummy_not)
+    assert {:ok, %Task{}} = dummy_not_result
+  end
+
+  test "dispatch multiple notifications asynchronously returns expected results" do
+    result = Ravenx.Test.TestNotification.dispatch_async(true)
+
+    {:ok, task} = Keyword.get(result, :dummy)
+    assert Task.await(task) == DummyStrategy.get_ok_result
+
+    {:ok, task} = Keyword.get(result, :dummy_not)
+    assert Task.await(task) == DummyStrategy.get_error_result
+  end
 end
