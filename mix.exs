6c222220c0d71c01dcab389f5cf7695fc0aabcd4
Óscar de Arriba <odarriba@gmail.com>
2018-02-17 12:52:07 +0100

    Remove dependencies and strategies except dummy

--- mix.exs
+++ mix.exs
@@ -40,10 +40,6 @@ defmodule Ravenx.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 2.0 or ~> 3.0"},
-      {:httpoison, "~> 1.0"},
-      {:bamboo, "~> 0.8"},
-      {:bamboo_smtp, "~> 1.4.0"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 0.4", only: :dev},
       {:credo, ">= 0.8.0", only: [:dev, :test]}
